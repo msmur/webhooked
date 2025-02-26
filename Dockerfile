8afecf98d8751c6116aa8ad4b38d39654ab591ed
Maahir Ur Rahman <msmaahirur@gmail.com>
2025-02-26 07:58:29 +0700

    build: add docker image and a (wip) prod compose file

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# Use official Python image
+FROM python:3.13
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy only requirements first (for better caching)
+COPY pyproject.toml poetry.lock ./
+
+# Install Poetry
+RUN pip install --no-cache-dir poetry
+
+# Install dependencies
+RUN poetry install --no-root
+
+# Copy the application code
+COPY . .
+
+# Expose the application port
+EXPOSE 3001
+
+# Run the application
+CMD ["poetry", "run", "fastapi", "run", "app/main.py", "--port", "3001"]
--- app/database.py
+++ app/database.py
@@ -1,10 +1,18 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://test:test@localhost:54321/test"
+from dotenv import load_dotenv
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+# Load environment variables from .env
+load_dotenv()
+
+# Get the database URL from the environment
+DB_CONNECTION_STRING = os.getenv("DB_CONNECTION_STRING")
+
+engine = create_engine(DB_CONNECTION_STRING, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
