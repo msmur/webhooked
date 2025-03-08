c2fbe60edd64400219f3f499e84d0863764a8d66
Maahir Ur Rahman <msmaahirur@gmail.com>
2025-03-08 16:48:18 +0700

    chore(*): disable sse while cloudflare proxy is investigated

--- app/webhooks/controller.py
+++ app/webhooks/controller.py
@@ -16,62 +16,62 @@ from ..hooks.repository import get_hook_or_throw
 
 router = APIRouter(prefix="/hooks/{hook_id}/webhooks", tags=["Webhooks"])
 
-connected_clients: Dict[str, Dict[str, asyncio.Queue]] = {}
-
-
-async def event_generator(queue: asyncio.Queue, hook_id: str, client_id: str):
-    try:
-        while True:
-            await queue.get()  # Wait for new webhook data
-            yield f"Received new webhooks!"
-    except asyncio.CancelledError:
-        logger.warning("Client disconnected, stopping event stream.")
-    except Exception as e:
-        logger.error(f"Unhandled exception in event generator: {e}")
-    finally:
-        # Remove the client from the connected clients list
-        logger.info(f"Removing client {client_id} from hook {hook_id}")
-        if hook_id in connected_clients:
-            if client_id in connected_clients[hook_id]:
-                del connected_clients[hook_id][client_id]
-
-
-@router.get("/{client_id}/events")
-async def sse(
-    hook_id: str = Path(...),
-    client_id: str = Path(...),
-):
-    """Endpoint for clients to listen for new webhook events."""
-    queue = asyncio.Queue()
-
-    # Store client in the dictionary
-    if hook_id not in connected_clients:
-        connected_clients[hook_id] = {}
-    connected_clients[hook_id][client_id] = queue
-
-    return EventSourceResponse(
-        event_generator(queue, hook_id, client_id),
-        headers={
-            "x-accel-buffering": "no",
-            "content-type": "text/event-stream",
-            "cache-control": "no-cache",
-            "Connection": "keep-alive",
-        },
-    )
-
-
-async def notify_clients(hook_id: str, event_data: str = "New webhook received!"):
-    """Send events only to clients subscribed to the specific hook ID."""
-    if hook_id in connected_clients:
-        logger.info(
-            f"Notifying clients for hook {hook_id}: {list(connected_clients[hook_id].keys())}"
-        )
-        # Iterate through all clients subscribed to this hook_id
-        for client_id, queue in connected_clients[hook_id].items():
-            try:
-                await queue.put(event_data)  # Send event to the client's queue
-            except Exception as e:
-                logger.error(f"Error notifying client {client_id}: {e}")
+# connected_clients: Dict[str, Dict[str, asyncio.Queue]] = {}
+#
+#
+# async def event_generator(queue: asyncio.Queue, hook_id: str, client_id: str):
+#     try:
+#         while True:
+#             await queue.get()  # Wait for new webhook data
+#             yield f"Received new webhooks!"
+#     except asyncio.CancelledError:
+#         logger.warning("Client disconnected, stopping event stream.")
+#     except Exception as e:
+#         logger.error(f"Unhandled exception in event generator: {e}")
+#     finally:
+#         # Remove the client from the connected clients list
+#         logger.info(f"Removing client {client_id} from hook {hook_id}")
+#         if hook_id in connected_clients:
+#             if client_id in connected_clients[hook_id]:
+#                 del connected_clients[hook_id][client_id]
+#
+#
+# @router.get("/{client_id}/events")
+# async def sse(
+#     hook_id: str = Path(...),
+#     client_id: str = Path(...),
+# ):
+#     """Endpoint for clients to listen for new webhook events."""
+#     queue = asyncio.Queue()
+#
+#     # Store client in the dictionary
+#     if hook_id not in connected_clients:
+#         connected_clients[hook_id] = {}
+#     connected_clients[hook_id][client_id] = queue
+#
+#     return EventSourceResponse(
+#         event_generator(queue, hook_id, client_id),
+#         headers={
+#             "x-accel-buffering": "no",
+#             "content-type": "text/event-stream",
+#             "cache-control": "no-cache",
+#             "Connection": "keep-alive",
+#         },
+#     )
+#
+#
+# async def notify_clients(hook_id: str, event_data: str = "New webhook received!"):
+#     """Send events only to clients subscribed to the specific hook ID."""
+#     if hook_id in connected_clients:
+#         logger.info(
+#             f"Notifying clients for hook {hook_id}: {list(connected_clients[hook_id].keys())}"
+#         )
+#         # Iterate through all clients subscribed to this hook_id
+#         for client_id, queue in connected_clients[hook_id].items():
+#             try:
+#                 await queue.put(event_data)  # Send event to the client's queue
+#             except Exception as e:
+#                 logger.error(f"Error notifying client {client_id}: {e}")
 
 
 @router.post(
@@ -126,7 +126,7 @@ async def receive_webhook(
 
     saved_webhook = insert_webhook(db, webhook_data)
 
-    asyncio.create_task(notify_clients(hook_id))
+    # asyncio.create_task(notify_clients(hook_id))
 
     return WebhookResponse(
         id=saved_webhook.id,
